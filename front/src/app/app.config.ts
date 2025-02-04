2d43eee010baaf94979f370d104145694f4a6c36
Cherifa3 <chaouchcherifa3@gmail.com>
2025-02-04 19:25:25 +0100

    register featre added

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './core/components/header/header.component';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-})
-export class AppComponent {
-  title = 'frontend';
-}
+import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { jwtInterceptor } from './core/inteceptors/jwt.interceptor';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch(), withInterceptors([jwtInterceptor])),
+  ],
+};
