266363179cd2070ad7e02f4c27a5bb4e825372e9
skan45 <gritliskander42@gmail.com>
2025-02-04 19:17:20 +0100

    Revert "features and shared done"
    This reverts commit 436b37918f663e67a81c657f7644366e5cf2d1bf.

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,19 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/components/header/header.component';
 
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {
-  provideHttpClient,
-  withFetch,
-  withInterceptors,
-} from '@angular/common/http';
-import { jwtInterceptor } from './core/inteceptors/jwt.interceptor';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideAnimationsAsync(),
-    provideHttpClient(withFetch(), withInterceptors([jwtInterceptor])),
-  ],
-};
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'frontend';
+}
